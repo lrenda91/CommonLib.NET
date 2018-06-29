278ad52d507a0886651ddbf0c8f1060b3ec52388
lrenda <renda@commitworld.com>
2018-06-29 15:19:30 +0200

    new project for unit test

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IMyDao interfaceProxy = TestDaoFactory.Get<IMyDao, MyDao>(null, null);
+            interfaceProxy.foo(3);
+
+            IMyDao classProxy = TestDaoFactory.Get<IMyDao>(null, null);
+            classProxy.foo(3);
+        }
+    }
+}
